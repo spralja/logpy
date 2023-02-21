0be87aeddbdece6c9494c5a885b786ccca437fd3
Robert Spralja <robert.spralja@gmail.com>
2023-02-21 17:19:32 +0100

    tests: Wrote tests for controller.Controller
    * test for the proper behavior of get_intersection

---
+++ tests/test_controller.py
@@ -0,0 +1,180 @@
+import unittest
+
+from .mock_controller import MockController
+
+from logpy.model import Entry
+
+from datetime import datetime, timezone
+
+class ControllerTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.controller = MockController(
+            Entry(
+                datetime(2022, 1, 1, 0, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 1, 1, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 2, 0, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 2, 1, 30, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 3, 9, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 3, 10, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 4, 10, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 4, 12, 0, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 5, 13, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 5, 14, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 6, 14, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 6, 15, 30, tzinfo=timezone.utc), 
+                'Personal'
+            )
+        )
+
+    def test_get_intersection_all(self):
+        interval = (
+            datetime(2021, 1, 1, tzinfo=timezone.utc),
+            datetime(2023, 1, 1, tzinfo=timezone.utc)
+        )
+
+        entries = self.controller.get_intersection(*interval)
+
+        expected = (
+            Entry(
+                datetime(2022, 1, 1, 0, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 1, 1, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 2, 0, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 2, 1, 30, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 3, 9, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 3, 10, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 4, 10, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 4, 12, 0, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 5, 13, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 5, 14, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 6, 14, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 6, 15, 30, tzinfo=timezone.utc), 
+                'Personal'
+            )
+        )
+
+        self.assertCountEqual(entries, expected)
+
+    def test_get_intersection_all_truncated(self):
+        interval = (
+            datetime(2022, 1, 1, 0, 30, tzinfo=timezone.utc),
+            datetime(2022, 1, 6, 15, tzinfo=timezone.utc)   
+        )
+
+        entries = self.controller.get_intersection(*interval)
+
+        expected = (
+            Entry(
+                datetime(2022, 1, 1, 0, 30, tzinfo=timezone.utc), 
+                datetime(2022, 1, 1, 1, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 2, 0, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 2, 1, 30, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 3, 9, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 3, 10, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 4, 10, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 4, 12, 0, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 5, 13, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 5, 14, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 6, 14, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 6, 15, 0, tzinfo=timezone.utc), 
+                'Personal'
+            )
+        )
+
+        self.assertCountEqual(entries, expected)
+
+    def test_get_intersection_none(self):
+        interval = (
+            datetime(2020, 1, 1, tzinfo=timezone.utc),
+            datetime(2021, 1, 1, tzinfo=timezone.utc)
+        )
+
+        entries = self.controller.get_intersection(*interval)
+
+        expected = tuple()
+
+        expected = ()
+
+        self.assertCountEqual(entries, expected)
+
+    def test_intersection_one(self):
+        interval = (
+            datetime(2022, 1, 1, 0, 30, tzinfo=timezone.utc), 
+            datetime(2022, 1, 1, 1, 0, tzinfo=timezone.utc), 
+        )
+
+        entries = self.controller.get_intersection(*interval)
+
+        expected = (
+            Entry(
+                datetime(2022, 1, 1, 0, 30, tzinfo=timezone.utc), 
+                datetime(2022, 1, 1, 1, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+        )
+
+        self.assertCountEqual(entries, expected)
+
+    def test_get_intersection_one_truncated(self):
+        interval = (
+            datetime(2022, 1, 2, 0, 30, tzinfo=timezone.utc), 
+            datetime(2022, 1, 2, 1, 15, tzinfo=timezone.utc), 
+        )
+
+        entries = self.controller.get_intersection(*interval)
+        
+        expected = (
+            Entry(
+                datetime(2022, 1, 2, 0, 30, tzinfo=timezone.utc), 
+                datetime(2022, 1, 2, 1, 15, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+        )
+
+        self.assertCountEqual(entries, expected)
+        
