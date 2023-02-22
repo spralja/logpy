e4e9d790c6b576fbd2a6e926329bb81a2a6a962f
Robert Spralja <robert.spralja@gmail.com>
2023-02-22 16:25:08 +0100

    tests: Wrote tests for model.MutatorController.delete_entry
    * test proper validation
    * test proper behaviour

--- tests/test_controller.py
+++ tests/test_controller.py
@@ -317,3 +317,65 @@ class MutattorController(unittest.TestCase):
         self.assertEqual(self.controller.data, expected_data)
         
         self.assertEqual(self.controller.mutations, expected_mutations)
+
+    def test_delete_entry_does_not_exist(self):
+        entry = Entry(
+            datetime(2023, 1, 1, tzinfo=timezone.utc),
+            datetime(2024, 1, 1, tzinfo=timezone.utc),
+            'Category'
+        )
+
+        with self.assertRaises(KeyError):
+            self.controller.delete_entry(entry)
+
+    def test_delete_entry(self):
+        entry = Entry(
+            datetime(2022, 1, 5, 13, 0, tzinfo=timezone.utc), 
+            datetime(2022, 1, 5, 14, 0, tzinfo=timezone.utc), 
+            'Work'
+        )
+        
+        expected_data = (
+            Entry(
+                datetime(2022, 1, 1, 0, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 1, 1, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 2, 0, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 2, 1, 30, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 3, 9, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 3, 10, 0, tzinfo=timezone.utc), 
+                'Work'
+            ),
+            Entry(
+                datetime(2022, 1, 4, 10, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 4, 12, 0, tzinfo=timezone.utc), 
+                'Personal'
+            ),
+            Entry(
+                datetime(2022, 1, 6, 14, 0, tzinfo=timezone.utc), 
+                datetime(2022, 1, 6, 15, 30, tzinfo=timezone.utc), 
+                'Personal'
+            )
+        )
+
+        expected_mutations = [
+            Mutation(
+                'destroyer',
+                Entry(
+                    datetime(2022, 1, 5, 13, 0, tzinfo=timezone.utc), 
+                    datetime(2022, 1, 5, 14, 0, tzinfo=timezone.utc), 
+                    'Work'
+                )
+            )
+        ]
+
+        self.controller.delete_entry(entry)
+
+        self.assertEqual(self.controller.data, expected_data)
+        self.assertEqual(self.controller.mutations, expected_mutations)
+
